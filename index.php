3476d6e0a605f637d8db8ef3d7d67ce38a97fe4f
Gregory36 <ggangstar36@gmail.com>
2022-12-03 20:58:27 +0400

    Ver 1.1 добавление bootstrap, php, базы данных

--- index.php
+++ index.php
@@ -15,6 +15,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <link rel="icon" href="source/logo_image.png">
     <title>Delote-Beauty</title>
@@ -37,6 +39,10 @@
             <button id="button_right_description"><div id="description_button"></div></button>
         </header>
         <main>
+            <div id="welcome_description">
+                <button id="logIn" type="button">Авторизоваться</button>
+                <h1 hidden="true" id="login_name">Привет</h1>
+            </div>
             <div class="main_description">
                 <h1>Салон красоты<br>"Delote-Beauty"<br>на Крестовском</h1>
             </div>
@@ -138,6 +144,38 @@
             <p>С 11:00 до 20:00 (Сб-Вс)</p>
         </div>
    </div>
-    <script src="script.js"></script>
+
+   <!-- bootstrap_modal -->
+   <div class="modal fade" id="formLoginModal" role="dialog" data-backdrop="static" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Авторизация</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Логин:</label>
+                            <input type="text" class="form-control" id="login" name="login" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="message-text" class="col-form-label">Пароль:</label>
+                            <input type="password" class="form-control" id="password" name="password" required>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
+                            <button type="submit" class="buttonLogInpr" name="enter" id="enter">Вход</button>
+                            <button type="submit" class="buttonRegpr" name="registration" id="registration">Регистрация</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="rightPanel.js"></script>
+    <script src="main.js"></script>
 </body>
 </html>
